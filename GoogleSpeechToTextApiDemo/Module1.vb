19bd0af360dca2038ad224df1c0a6f2649340a60
Yiannis Kapodistrias <mcgraf@gmail.com>
2019-01-01 14:13:36 +0200

    1

--- GoogleSpeechToTextApiDemo/Module1.vb
+++ GoogleSpeechToTextApiDemo/Module1.vb
@@ -1,7 +1,21 @@
-﻿Module Module1
+﻿Imports Google.Cloud.Speech.V1
+Imports System
 
-    Sub Main()
+Namespace SpeechToTextApiDemo
 
-    End Sub
+    Module Module1
 
-End Module
+        Sub Main()
+            Dim speech = SpeechClient.Create()
+            Dim config = New RecognitionConfig With {.LanguageCode = LanguageCodes.Greek.Greece, .Model = vbUseDefault}
+            Dim audio = RecognitionAudio.FromStorageUri("gs//speech-demo-2/Vocaroo3.wav")
+            Dim responce = speech.Recognize(config, audio)
+            For Each result In responce.Results
+                For Each alternative In result.Alternatives
+                    Console.WriteLine(alternative.Transcript)
+                Next
+            Next
+        End Sub
+
+    End Module
+End Namespace
